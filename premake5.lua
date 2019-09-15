181c67ac6a9063679ee5dbb5fff49d2df05550d1
cqncpdp <arthurgarreau.web@gmail.com>
2019-09-15 11:21:34 +0200

    add precompiled header

--- premake5.lua
+++ premake5.lua
@@ -18,6 +18,9 @@ project "heckel"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+	pchheader "hzpch.h"
+	pchsource "heckel/src/hzpch.cpp"
+
 	files
 	{
 		"%{prj.name}/src/**.h",
